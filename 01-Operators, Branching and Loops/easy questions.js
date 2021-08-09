cb6a9b3d3e8e344920177d75a9134887d43e9922
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-08-09 15:26:27 +0530

    practice started

---
+++ 01-Operators, Branching and Loops/easy questions.js
@@ -0,0 +1,9 @@
+// Q1.) Write a program to add 5 numbers.
+
+function add(num1,num2,num3,num4,num5){
+    return sum = num1+num2+num3+num4+num5;
+}
+
+add(5,13,7,21,48);
+console.log("Sum = ", sum);
+
