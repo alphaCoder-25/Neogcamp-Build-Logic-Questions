bb8cb51e9b13de88fb888ca73b01d2d4a5103692
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-09-28 19:12:06 +0530

    some changes

--- 01-Operators, Branching and Loops/easy questions.js
+++ 01-Operators, Branching and Loops/easy questions.js
@@ -24,52 +24,46 @@ if(input % 2 === 0){
 
 const num1 = 129; num2 = 251;
 
-if(num1 > num2 ){
-    console.log(num1, " is maximum &", num2, " is minimum");
-}else{
-    console.log(num2, " is maximum &", num1, " is minimum");
+if(num1 > num2){
+    console.log(`${num1} is maximum and ${num2} is minimum`);
+} else {
+    console.log(`${num2} is maximum and ${num1} is minimum`);
 }
 
 // Q4.) Write a program to find the maximum out of 3 given numbers.
 
 const num1 = 8; num2 = 23; num3 = 17;
 
-if (num1 > num2 && num1 > num3) {
-    console.log("The maximum number is: ", num1);
-} else if (num2 > num1 && num2 > num3) {
-    console.log("The maximum number is: ", num2);
-} else if (num3 > num1 && num3 > num2) {
-    console.log("The maximum number is: ", num3);
-} else {
-    console.log("All numbers are Equal");
+if(num1 > num2 && num1 > num3){
+    console.log(`${num1} is maximum`);
+} else if(num2 > num1 && num2 > num3){
+    console.log(`${num2} is maximum`)
+} else if(num3 > num1 && num3 > num2){
+    console.log(`${num3} is maximum`);
 }
 
 // Q5.) Write a program to find the minimum out of 3 given numbers.
 
 const num1 = 35; num2 = 29; num3 = 46;
 
-if (num1 < num2 && num1 < num3) {
-    console.log("The minimum number is: ", num1);
-} else if (num2 < num1 && num2 < num3) {
-    console.log("The minimum numnber is: ", num2);
-} else if (num3 < num1 && num3 < num2) {
-    console.log("The minimum number is: ", num3);
-} else {
-    console.log("All numbers are Equal");
+if(num1 < num2 && num1 < num3){
+    console.log(`${num1} is minimum`);
+} else if(num2 < num1 && num2 < num3){
+    console.log(`${num2} is minimum`)
+} else if(num3 < num1 && num3 < num2){
+    console.log(`${num3} is minimum`);
 }
 
 // Q6.) Write a program to take a month as an input from the user and find out whether the month has 31 days or not.
 
 var readlineSync = require('readline-sync');
 const month = readlineSync.question("Enter the month ");
-console.log("You entered ", month);
- 
+console.log(`You entered ${month}`);
+
 if( month === 'january' || month === 'march' || month === 'may' || month === 'july' || month === 'august' || month === 'october' || month === 'december' ) {
-    console.log("The given month has 31 days. ");
+    console.log("The month has 31 days.");
 } else {
-    console.log("The given month has no 31 days. ");
+    console.log("The month doesn't have 31 days. ");
 }
-
-
 
 
